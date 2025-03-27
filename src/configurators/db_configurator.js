a5342ea53fe7784b31493cff56d5eb60b6493621
FabriceRODRIGUEZ <fabricerod31@gmail.com>
2025-03-27 09:27:50 +0100

    Configuration de la base de données

--- src/configurators/db_configurator.js
+++ src/configurators/db_configurator.js
@@ -1,3 +1,85 @@
+import dotenv from "dotenv"
+import pg from "pg"
+
+import {users_creation, characters_creation} from "./creation_queries.js"
+
+
+export default class DbConfigurator {
+
+    connect() {
+        dotenv.config()
+        console.log(`Using database ${process.env.DB_NAME}`)
+        this.db = new pg.Pool({
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME
+        })
+    }
+
+    async seed_database() {
+        console.log("Seeding database...")
+        try {
+            await this.db.query(users_creation)
+        } catch(error) {
+            // TABLE ALREADY EXISTS (https://www.postgresql.org/docs/8.2/errcodes-appendix.html)
+            if (error.code !== "42P07") {
+                console.log(error)
+            }
+        }
+        try {
+            await this.db.query(characters_creation)
+        } catch(error) {
+            // TABLE ALREADY EXISTS (https://www.postgresql.org/docs/8.2/errcodes-appendix.html)
+            if (error.code !== "42P07") {
+                console.log(error)
+            }
+        }
+    }
+
+}
+
+// - - -
+
+// module.exports = async (userAccountService, carService) => {
+//     return new Promise(async (resolve, reject) => {
+//         console.log("Seeding database...")
+//         try {
+//             await userAccountService.dao.db.query("CREATE TABLE useraccount(id SERIAL PRIMARY KEY, displayname TEXT NOT NULL, login TEXT NOT NULL, password TEXT NOT NULL)")
+//             await carService.dao.db.query(`CREATE TABLE car (id SERIAL PRIMARY KEY, make TEXT, model TEXT, 
+//                                         isrunning BOOLEAN, price NUMERIC, builddate DATE,
+//                                         useraccount_id INTEGER REFERENCES useraccount(id))`)
+//             resolve()
+//         }catch(e)  {
+//             if (e.code === "42P07") { // TABLE ALREADY EXISTS https://www.postgresql.org/docs/8.2/errcodes-appendix.html
+//                 resolve()
+//             } else {
+//                 console.log(e)
+//                 reject(e)
+//             }
+//         }
+//     })
+// }
+
+// module.exports = class BaseDAO {
+//     constructor(db, tablename) {
+//         this.db = db
+//         this.tablename = tablename
+//     }
+//     delete(id) {
+//         return this.db.query(`DELETE FROM ${this.tablename} WHERE id=$1`, [id])
+//     }
+//     getById(id) {
+//         return new Promise((resolve, reject) =>
+//             this.db.query(`SELECT * FROM ${this.tablename} WHERE id=$1`, [ id ])
+//                 .then(res => resolve(res.rows[0]) )
+//                 .catch(e => reject(e)))
+//     }
+// }
+
+// - - -
+
 // import { Sequelize } from "sequelize"
 // import dotenv from "dotenv"
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,28 @@
+import dotenv from "dotenv"
+import express from "express"
+
+import DbConfigurator from "./configurators/db_configurator.js"
+
+
+export default class Server {
+
+    start() {
+        dotenv.config()
+        const app = express()
+
+        const db_configurator = new DbConfigurator()
+        db_configurator.connect()
+        db_configurator.seed_database()
+
+        app.listen(process.env.PORT, () =>
+            console.log(`Server started on port ${process.env.PORT}`)
+        )
+    }
+
+}
+
+// - - -
+
 // import dotenv from "dotenv"
 // import express from "express"
 // import cors from "cors"
@@ -29,3 +54,43 @@
 //     }
 
 // }
+
+// - - -
+
+// const pg = require('pg')
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const morgan = require('morgan')
+// require('dotenv').config();
+
+// const CarService = require("./services/car")
+// const UserAccountService = require("./services/useraccount")
+
+// const app = express()
+// app.use(bodyParser.urlencoded({ extended: false })) // application/x-www-form-urlencoded
+// app.use(bodyParser.json()) // application/json
+// app.use(cors())
+// app.use(morgan('combined')); // toutes les requêtes HTTP dans le log du serveur
+
+// const port = process.env.PORT || 3333;
+// const dsn = process.env.CONNECTION_STRING
+
+// console.log(`Using database ${dsn}`)
+// const db = new pg.Pool({ connectionString:  dsn})
+
+// const carService = new CarService(db)
+// const userAccountService = new UserAccountService(db)
+// const jwt = require('./jwt')(userAccountService)
+// require('./api/car')(app, carService, jwt)
+// require('./api/useraccount')(app, userAccountService, jwt)
+
+// const seedDatabase = async () => require('./datamodel/seeder')(userAccountService, carService)
+// if (require.main === module) {
+//     seedDatabase().then( () =>
+//         app.listen(port, () =>
+//             console.log(`Listening on the port ${port}`)
+//         )
+//     )
+// }
+// module.exports= { app, seedDatabase, userAccountService }
