b7dbc1ac4a38bfdaf62213dc334563de138867c8
FabriceRODRIGUEZ <fabricerod31@gmail.com>
2025-06-11 19:24:43 +0200

    Mise à jour

--- src/routers/character_router.js
+++ src/routers/character_router.js
@@ -24,7 +24,8 @@ export default class CharacterRouter {
         const characterValidator = new CharacterValidator(db)
         const controller = new CharacterController(db)
 
-        this.router.get("/characters", authValidator.authenticate, controller.getAllCharacters)
+        // this.router.get("/characters", authValidator.authenticate, controller.getAllCharacters)
+        this.router.get("/characters", authValidator.authenticate, controller.getCharacters)
         this.router.get("/characters/:id", authValidator.authenticate, controller.getCharacter)
         this.router.post("/characters", authValidator.authenticate,
             characterValidator.validateAddCharacter, controller.addCharacter)
